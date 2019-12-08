20069ab145000108d4576e71607be415955397e6
Mathijs Bakker <mathijs_bakker@me.com>
2019-12-08 21:21:23 +0100

    Mods to object types and Rider related stuff

--- Unity Project/Assets/Package B/Code/DummyScriptB.cs
+++ Unity Project/Assets/Package B/Code/DummyScriptB.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DummyScript : MonoBehaviour
+public class DummyScriptB : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
